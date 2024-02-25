40fef61904878cdbc59af895b731bc7a6abf769b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-02-25 23:36:13 +0200

    ✅ Feat(TS) Homework part 1 complete

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,20 +1,25 @@
-import React, { useEffect, useRef } from 'react';
+import React, { ReactNode, useEffect, useRef } from 'react';
+
+type Props = {
+  children: ReactNode;
+  onContentEndVisible: () => void;
+};
 
 // Опишіть Props
 export function Observer({ children, onContentEndVisible }: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+  const endContentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
+    const options: IntersectionObserverInit = {
       rootMargin: '0px',
       threshold: 1.0,
       root: null,
     };
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
         if (entry.intersectionRatio > 0) {
           onContentEndVisible();
           observer.disconnect();
