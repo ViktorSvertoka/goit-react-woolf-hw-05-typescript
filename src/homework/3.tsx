e789f1cb571dbfb2373e856d218465b6aa1a6778
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-02-25 23:56:19 +0200

    ✅ Feat(TS) Homework part 3 complete

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 export function FormComponent() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  return (
-    <input type="text" value={value} onChange={handleChange} />
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(event.target.value);
+    },
+    [],
   );
+
+  return <input type="text" value={value} onChange={handleChange} />;
 }
